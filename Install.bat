0db3aea9bdc7d383c4c8d560e903bdc6fc455852
Vincent Timbro <43245289+Tamayuzu@users.noreply.github.com>
2020-08-26 13:42:46 -0400

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,9 @@
+npm config set proxy http://proxy.prv:80
+
+npm config set https-proxy http://proxy.prv:443
+
+npm i http-server
+
+echo test
+
+pause
---
+++ activate server.bat
@@ -0,0 +1,6 @@
+
+cd /d %~dp0
+
+call "C:\Program Files\nodejs\nodevars.bat"
+
+http-server
