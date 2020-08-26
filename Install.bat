fcd0e5258abde6dea079d467be43782ba800d972
Vincent Timbro <43245289+Tamayuzu@users.noreply.github.com>
2020-08-26 14:17:18 -0400

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,8 +1,8 @@
-npm config set proxy http://proxy.prv:80
+call npm config set proxy http://proxy.prv:80
 
-npm config set https-proxy http://proxy.prv:443
+call npm config set https-proxy http://proxy.prv:443
 
-npm i http-server
+call npm i http-server
 
 echo test
 
